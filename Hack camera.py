3b30c1eaa04ea9fe79cc39fbdae9ecaf8897bf81
kingmon9900 <109856606+kingmon9900@users.noreply.github.com>
2022-07-24 13:33:07 +0430

    Update Hack camera.py

--- Hack camera.py
+++ Hack camera.py
@@ -1,48 +1,36 @@
-import os 
+import os,time,random
 os.system("clear")
-print("wellcome pakg me for hack")
+print("wellcome pkg me for hack")
 x= int(input("1-2-3-4-5? "))
 if x==1:
-  print("http://173.14.183.116:80")
-  print("http://47.23.137.138:1024")
-  print("http://173.14.105.148:80")
-  print("http://174.79.171.156:8001")
-  print("http://153.10.241.145:80")
-  print("http://192.33.141.123:80")
-  print("serch in google")
+    q=["http://47.23.137.138:1024","http://173.14.105.148:80","http://174.79.171.156:8001","http://153.10.241.145:80","http://192.33.141.123:80","http://173.14.183.116:80"]
+    w=random.choice(q)
+    print("wait for opening Site")
+    time.sleep("3")
+    os.system(f"xdg-open {w}")
 if x==2:
-  print ("http://50.122.133.122:3000")
-  print("http://162.233.208.123:5001")
-  print("http://128.196.100.131:80")
-  print("http://166.156.152.133:5001")
-  print("http://166.156.152.135:5001") 
-  print("http://173.8.2.109:80")
-  print("serch in google")
+    o=["http://50.122.133.122:3000","http://162.233.208.123:5001","http://128.196.100.131:80","http://166.156.152.133:5001","http://166.156.152.135:5001","http://173.8.2.109:80"]
+    w=random.choice(o)
+    print("wait for opening Site")
+    time.sleep("3")
+    os.system(f"xdg-open {w}")
 if x==3:
-  print("http://107.207.221.109:5001")
-  print ("http://128.252.39.112:80")
-  print("http://173.224.21.108:80")
-  print("http://98.173.199.86:8081")
-  print("http://129.65.19.93:80")
-  print("http://166.142.213.90:5001")
-  print("serch in google")
+    q=["http://107.207.221.109:5001","http://128.252.39.112:80","http://173.224.21.108:80","http://98.173.199.86:8081","http://129.65.19.93:80","http://166.142.213.90:5001"]
+    w=random.choice(q)
+    print("wait for opening Site")
+    time.sleep("3")
+    os.system(f"xdg-open {w}")
 if x==4:
-  print("http://72.20.74.91:80")
-  print("http://63.142.200.93:8001")
-  print("http://107.220.54.61:5001")
-  print ("http://64.139.100.67:60001")
-  print("http://137.99.198.55:80")
-  print("serch in google")
+    q=["http://72.20.74.91:80","http://63.142.200.93:8001","http://107.220.54.61:5001","http://64.139.100.67:60001","http://137.99.198.55:80"]
+    w=random.choice(q)
+    print("wait for opening Site")
+    time.sleep("3")
+    os.system(f"xdg-open {w}")
 if x==5:
-  print("http://68.177.73.106:82")
-  print("http://50.244.219.100:80")
-  print("http://74.93.189.58:82")
-  print("http://128.252.39.27:80")
-  print("http://104.58.192.15:80")
-  print("http://209.124.145.124:81")
-  print("http://208.105.178.210:1024")
-  print("http://128.252.163.102:80")
-  print("http://75.146.47.252:80")
-  print("http://75.146.47.253:80")
-  print("serch in google")
+    q=["http://68.177.73.106:82","http://50.244.219.100:80","http://74.93.189.58:82","http://128.252.39.27:80","http://104.58.192.15:80","http://209.124.145.124:81","http://208.105.178.210:1024","http://128.252.163.102:80","http://75.146.47.252:80","http://75.146.47.253:80"]
+    w=random.choice(q)
+    print("wait for opening Site")
+    time.sleep("3")
+    os.system(f"xdg-open {w}")
+
   
